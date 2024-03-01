28f673ade9a3b5136234562ed90df3ab5d4616af
Animal Janwar <160995009+Githubsoniaryan2@users.noreply.github.com>
2024-03-01 12:24:31 +0530

    Update clients.py

--- AdityaHalder/modules/clients/clients.py
+++ AdityaHalder/modules/clients/clients.py
@@ -52,21 +52,21 @@ async_dirs()
     
 
 app = Client(
-    name = "AdityaHalder",
+    name = "AnimalJanwar",
     api_id = API_ID,
     api_hash = API_HASH,
     session_string = STRING_SESSION,
 )
 
 ass = Client(
-    name = "AdityaPlayer",
+    name = "AnimalPlayer",
     api_id = API_ID,
     api_hash = API_HASH,
     session_string = SESSION_STRING,
 )
 
 bot = Client(
-    name = "AdityaServer",
+    name = "AnimalServer",
     api_id = API_ID,
     api_hash = API_HASH,
     bot_token = BOT_TOKEN,
@@ -113,8 +113,8 @@ async def run_async_clients():
     except:
         pass
     try:
-        await app.join_chat("AdityaServer")
-        await app.join_chat("AdityaDiscus")
+        await app.join_chat("itsRareBeautySelenaGomez")
+        await app.join_chat("itsRareBeautySelenaGomez")
     except:
         pass
     if SESSION_STRING:
@@ -126,8 +126,8 @@ async def run_async_clients():
         except:
             pass
         try:
-            await app.join_chat("AdityaServer")
-            await app.join_chat("AdityaDiscus")
+            await app.join_chat("itsRareBeautySelenaGomez")
+            await app.join_chat("itsRareBeautySelenaGomez")
         except:
             pass
     LOGGER.info("Starting Helper Robot ...")
